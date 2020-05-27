9cc057df8b724898be6c7514e07ac8cf99388e5d
hechao <hechao_vendor@sensetime.com>
2020-05-27 22:43:09 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/greet/<name>')
+@app.route('/greet',defaults = {"name":"小秋哥"})
+def index(name):
+    return "hello," + name
+
+
+if __name__ == "__main__":
+    app.run(debug=app.config['DEBUG'])
